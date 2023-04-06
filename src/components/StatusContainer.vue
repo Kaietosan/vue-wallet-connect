879d0c4520bbdd049ea374a3613a5d7591ff8ff1
Kaieto <shibugaki1230@gmail.com>
2023-04-06 18:07:49 +0900

    statusContainer.jsの追加とそれに伴う変更

--- src/components/StatusContainer.vue
+++ src/components/StatusContainer.vue
@@ -1,19 +1,36 @@
 <template>
     <div class="hello">
         <h1>Welcome to Your Vue.js Dapp</h1>
-        <div>
-            <button class="button">Connected</button>
+        <div v-if="state.status">
+            <button @click="connectUserWallet" class="button">Connected</button>
+            <h3>Address: {{stete.address}}</h3>
+            <h3>ChainId: {{state.chainId}}</h3>
             <button class="disconnect__button">Disconnect</button>
 
         </div>
-        <button class="button">Connect Wallet</button>
+        <button v-else @click="connectUserWallet" class="button">Connect Wallet</button>
 
     </div>
 </template>
 
 <script>
+import connect from '../composables/connect/index';
 export default {
-    name:'StatusContainer'
+  name: 'StatusContainer',
+  setup: () => {
+    const { connectWalletConnect, disconnectWallet, state } = connect();
+    const connectUserWallet = async () => {
+      await connectWalletConnect();
+    };
 
+    const disconnectUser = async() => {
+      await disconnectWallet()
+    }
+    return {
+      connectUserWallet,
+      disconnectUser,
+      state
+    }
+  }
 }
 </script>
